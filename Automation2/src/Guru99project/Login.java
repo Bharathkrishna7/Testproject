895d2a2eeb4bf38bb1a60a8d663c62e924013275
bharathkrishna7 <bharathkrishna7777@gmail.com>
2019-08-10 17:35:32 +0530

    Push

---
+++ Automation2/src/Guru99project/Login.java
@@ -0,0 +1,19 @@
+package Guru99project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Login 
+{
+	public static void main(String[]args)
+	{
+		FirefoxDriver ff=new FirefoxDriver();
+		ff.get("http://www.demo.guru99.com/V4/");
+		ff.findElement(By.xpath("//input[@type='text']")).sendKeys("mngr215403");
+		ff.findElement(By.name("password")).sendKeys("vupanyb");
+		ff.findElement(By.xpath("//input[@type='submit']")).click();
+	}
+	 
+	
+
+}
